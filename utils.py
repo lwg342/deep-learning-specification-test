82b940440ac765288feca7b7e3d7284a8beddb3e
Lumin <47528654+lwg342@users.noreply.github.com>
2021-04-20 10:49:31 +0100

    now can compute C values at an array of points

--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 def C_resid(e0 , e1, y, T, option = None):
-    d0 = (y - e0).clip()
-    d1 = (y - e1).clip()
-    C = 1/np.sqrt(T) * ((d0 - d1).sum())
+    y_mat = np.outer(y, np.ones(T))
+    d0 = (y_mat - np.outer(np.ones(len(y)), e0)).clip(0)
+    d1 = (y_mat - np.outer(np.ones(len(y)), e1)).clip(0)
+    C = 1/np.sqrt(T) * ((d0 - d1).sum(1))
     return C
 
-def test_statistic(C, method = "CM"):
-    rslt = None
+
+def test_statistic(T, sigma_hat=1, method="CM"):
+    if method == "CM":
+        rslt = 3/sigma_hat /T 
+    
+    
     return rslt
