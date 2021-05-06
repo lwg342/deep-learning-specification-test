f33f8e68cdc082268aaba27dba7464e15807c9b5
Lumin <47528654+lwg342@users.noreply.github.com>
2021-05-06 17:04:22 +0100

    create a class to modulize codes

--- test_4.py
+++ test_4.py
@@ -1,85 +1,21 @@
 # %%
 import torch
-import torch.nn.functional as F
-import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-
+import utils_simulation
+import utils
 # %%
 # torch.manual_seed(1)    # reproducible
+N = 200 
+x,y,e = utils_simulation.generate_sample(N, k = 4, sigma = 0.5) 
+plt.scatter(x[:,0], y)
+plt.scatter(x[:,1], y)
 
-N = 200
-rng = np.random.default_rng()
-x1 = torch.from_numpy(rng.normal(0, 1, N))
-x2 = torch.from_numpy(rng.normal(0, 1, N))
-x3 = torch.from_numpy(rng.normal(0, 1, N))
-x4 = torch.from_numpy(rng.normal(0, 1, N))
-e = 0.2*torch.randn(N)
-
-
-def true_output(x1, x2, x3, x4):
-    # y = x1.pow(2) + 2* torch.sin(x2) + x1*x2 + x3 * x4
-    y = x1 + 2 * (x2) + 3 * x3 + 4*x4
-    return y
-
-
-y = (true_output(x1, x2, x3, x4) + e).unsqueeze(-1).float()
-
-plt.scatter(x1, y)
-plt.scatter(x2, y)
-
-
-# %%
-x1.unsqueeze(-1).float()
-x2.unsqueeze(-1).float()
-x3.unsqueeze(-1).float()
-x4.unsqueeze(-1).float()
-x = torch.stack((x1,x2, x3, x4)).transpose(0,1).float()
-
-
-class Net(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
-        self.f1 = nn.Linear(4, 5)
-        self.f2 = nn.Linear(5, 5)
-        self.f3 = nn.Linear(5, 5)
-        self.f4 = nn.Linear(5, 5)
-        self.predict = nn.Linear(5, 1)
-        
-
-    def forward(self, x):
-        x = F.relu(self.f1(x))
-        x = F.relu(self.f2(x))
-        x = F.relu(self.f3(x))
-        x = F.relu(self.f4(x))
-        out = self.predict(x)
-        return out
-
-
-net = Net()
-print(net(x)[0:5])
+M = utils.wl_nn(utils.MLP4, lr = 0.0001)
 # %%
 
-# optimizer = torch.optim.SGD(net.parameters(), lr=0.0005)
-optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
-loss_func = torch.nn.MSELoss()  
-# %%
-
-for t in range(10000):
-    prediction = net(x)     # input x and predict based on x
-
-    loss = loss_func(prediction, y)     # must be (1. nn output, 2. target)
-
-
-    optimizer.zero_grad()   # clear gradients for next train
-    loss.backward()         # backpropagation, compute gradients
-    optimizer.step()        # apply gradients
-
-    if t % 100 == 0:
-        print(loss)
-# %%
-plt.scatter(y,net(x).detach().numpy())
+M.net_update(x,y,batch_size=10000)
+plt.scatter(y,M.net(x).detach())
 
 # %%
 
@@ -88,28 +24,25 @@ from wl_regression import OLS
 z0 = OLS(x.numpy(), y.numpy()).y_hat()
 e0 = (z0 - y.detach().numpy()[:,0])
 
-z = net(x)
+z = M.net(x)
 e1 = (z-y).transpose(1,0).detach().numpy()
 
 plt.scatter(y,e0)
 plt.scatter(y,e1)
 
-from wl_regression import loc_poly
-ll_z = loc_poly(y.numpy(), x.numpy(), x.detach().numpy())
-e2 = (ll_z - y.detach().numpy()[:,0])
+# from wl_regression import loc_poly
+# ll_z = loc_poly(y.numpy(), x.numpy(), x.detach().numpy())
+# e2 = (ll_z - y.detach().numpy()[:,0])
 
 # %%
-import utils 
 from importlib import reload  
 reload(utils)
-C_resid = utils.C_resid
-C, d0, d1= C_resid(e0, e1, e0, N,"full")
+C, d0, d1 = utils.C_resid(e0, e1, e0, N, "full")
 plt.plot(C)
-print(C)
 # %%
 sigma_hat = utils.compute_w(x,y,torch.tensor(e0),torch.tensor(e1),N)[1]
-test_statistic = utils.test_statistic
-rslt = test_statistic(C, N, sigma_hat)
+rslt =utils.test_statistic(C, N, sigma_hat)
 print(sigma_hat, rslt)
 # %%
 # %%
+cc =np.sort(np.random.chisquare(1, 100))
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 
+
 def C_resid(e0 , e1, y, T, option = None):
     y_mat = np.outer(y, np.ones(T))
     d0 = (y_mat - np.outer(np.ones(len(y)), e0)).clip(0)
@@ -31,3 +32,40 @@ def test_statistic(C, T, sigma_hat, method="CM"):
     if method == "CM":
         rslt = 3/(sigma_hat)**2 / T * np.inner(C, C)
     return rslt
+
+# %% This is about constructing the neural net
+class MLP4(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.f1 = torch.nn.Linear(4, 5)
+        self.f2 = torch.nn.Linear(5, 5)
+        self.f3 = torch.nn.Linear(5, 5)
+        self.f4 = torch.nn.Linear(5, 5)
+        self.predict = torch.nn.Linear(5, 1)
+
+    def forward(self, x):
+        x = torch.nn.functional.relu(self.f1(x))
+        x = torch.nn.functional.relu(self.f2(x))
+        x = torch.nn.functional.relu(self.f3(x))
+        x = torch.nn.functional.relu(self.f4(x))
+        out = self.predict(x)
+        return out
+
+
+class wl_nn():
+    def __init__(self, model=MLP4, lr=0.0001):
+        self.net = model()
+        self.optimizer = torch.optim.Adam(self.net.parameters(), lr= lr)
+        self.loss_func = torch.nn.MSELoss()
+        self.previous_loss = 0
+
+    def net_update(self, x, y, batch_size = 10000):
+        for t in range(batch_size):
+            prediction = self.net(x)     # input x and predict based on x
+            loss = self.loss_func(prediction, y)     # must be (1. nn output, 2. target)
+            loss.backward()         # backpropagation, compute gradients
+            self.optimizer.step()        # apply gradients
+            self.optimizer.zero_grad()   # clear gradients for next train
+        self.loss = loss
+
+    
